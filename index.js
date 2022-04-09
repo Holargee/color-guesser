f8623dad19f13791270175b1debdf951d336dc56
Holargee <101959350+Holargee@users.noreply.github.com>
2022-04-09 04:33:25 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+function color(){
+				var a = Math.floor(Math.random() * 225);
+				var b = Math.floor(Math.random() * 225);
+				var c = Math.floor(Math.random() * 225);
+document.getElementById("circle").style.backgroundColor = "rgb("+a+","+b+","+c+")";
+document.getElementById("red").value = a;
+document.getElementById("blue").value = b;
+document.getElementById("green").value = c;
+}
+function color2(){
+	var a =		document.getElementById("red");
+  var b = document.getElementById("blue");
+  var c = document.getElementById("green");
+document.getElementById("circle").style.backgroundColor ="rgb("+a.value+","+b.value+"," +c.value+")";
+}	
+function dark(){
+var a = document.getElementById("body").style;
+var b = document.getElementById("body").style;
+var c = document.getElementById("dark");
+if(c.value=="dark mode"){a.backgroundColor="rgb(15,15,15)";a.color="white";c.value="light mode"}
+else{a.backgroundColor="white";a.color="black";c.value="dark mode"}
+}				
